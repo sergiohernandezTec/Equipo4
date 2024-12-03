f0914d49d3d7fa133c80724285fbb657affe9482
Angel Camilo <A01426143@tec.mx>
2024-12-02 23:31:50 -0600

    Venta y Turno

---
+++ Venta.cpp
@@ -0,0 +1,15 @@
+#include "Venta.h"
+#include <iostream>
+using namespace std;
+
+Venta::Venta(string clienteVenta, string productoVenta, int cantidadVenta, float totalVenta) {
+    cliente = clienteVenta;
+    producto = productoVenta;
+    cantidad = cantidadVenta;
+    total = totalVenta;
+}
+
+void Venta::mostrarVenta() {
+    cout << "Cliente: " << cliente << ", Producto: " << producto
+         << ", Cantidad: " << cantidad << ", Total: $" << total << endl;
+}
